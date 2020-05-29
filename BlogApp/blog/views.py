de5479b0ea1520f7e72ad05a70662fcbf1009187
splAcharya <spl.acharya@outlook.com>
2020-05-29 00:32:39 -0500

    stated working on templates

--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+posts = [
+
+        {
+            "author": "Swapnil",
+            "title" : "Post 1",
+            "content" : "First Post Yay!!!",
+            "date_posted": "May 29th 2020"
+        },
+
+        
+        {
+            "author": "Acharya",
+            "title" : "Post 2",
+            "content" : "Second Post Yay!!!",
+            "date_posted": "May 30th 2020"
+        }
+    ]
+
+
 
 def index(request):
-    return render(request,"blog/index.html")
+    context = {
+            "posts": posts
+        }
+    return render(request,"blog/index.html",context)
+
 
 
 def home(request):
