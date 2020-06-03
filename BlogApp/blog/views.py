07487f7fa6fd6280b7e5ad5f55e512d946710914
splAcharya <splAcharya@outlook.com>
2020-06-03 16:12:27 -0500

    made data models using code firt approach, made migrations and applied migrations, passed database models to a template, registered POst models in admin site

--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,33 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 # Create your views here.
-posts = [
-
-        {
-            "author": "Swapnil",
-            "title" : "Post 1",
-            "content" : "First Post Yay!!!",
-            "date_posted": "May 29th 2020"
-        },
-
-        
-        {
-            "author": "Acharya",
-            "title" : "Post 2",
-            "content" : "Second Post Yay!!!",
-            "date_posted": "May 30th 2020"
-        }
-    ]
-
 
 
 
 def home(request):
     context = {
             "title": "Home",
-            "posts": posts
+            "posts": Post.objects.all()
         }
     return render(request,"blog/home.html",context)
 
