2cc6dea3723fe07a5e9318bf6f0bb074b66e9f3b
splAcharya <splAcharya@outlook.com>
2020-06-10 18:35:03 -0500

    added profile image option and other profile stuff

--- BlogApp/BlogApp/settings.py
+++ BlogApp/BlogApp/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media") #no matter file system or os, a folder called media will be created
+MEDIA_URL = "/media/"
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
--- BlogApp/BlogApp/urls.py
+++ BlogApp/BlogApp/urls.py
@@ -21,8 +21,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
@@ -35,3 +35,7 @@ urlpatterns = [
     path("blog/",include("blog.urls")),
     path("users/",include("users.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
