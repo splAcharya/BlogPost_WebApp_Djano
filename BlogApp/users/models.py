c217b09d85802c0d284b697d1c59dadfb50ffa59
splAcharya <splAcharya@outlook.com>
2020-06-11 13:41:56 -0500

    image added in blog posts

--- BlogApp/users/models.py
+++ BlogApp/users/models.py
@@ -16,6 +16,6 @@ class Profile(models.Model):
         super().save()
         img = Image.open(self.image.path)
         if((img.height > 300) or (img.width > 300)):
-            output_size=(300,300)
+            output_size=(125,125)
             img.thumbnail(output_size)
             img.save(self.image.path)
