f35b9d669fd5e8c20a049492b54443ff6e69402e
splAcharya <spl.acharya@outlook.com>
2020-08-09 14:39:02 -0500

    added blog create, update, delete views, however the redirect is not workign

--- BlogApp/blog/views.py
+++ BlogApp/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, 
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 
 # Create your views here.
 
@@ -13,6 +19,68 @@ def home(request):
         }
     return render(request,"blog/home.html",context)
 
+
+
+class PostListView(ListView):
+    model = Post
+    #by defaul the postlistview looks for template in format <app>/<model>_<viewtype>.html
+    #for example: blog/post_list.html
+
+    #override the default template for this listview
+    template_name = "blog/home.html"
+    context_object_name = "posts" #from the context passed in home function
+    ordering = ["-date_posted"] #the - in "-date_posted" reverses the list
+
+
+class PostDetailView(DetailView):
+    model = Post
+    #by defaul the postlistview looks for template in format <app>/<model>_<viewtype>.html
+    #for example: blog/post_list.html
+
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ["title","content"]
+    #by defaul the postlistview looks for template in format <app>/<model>_<viewtype>.html
+    #for example: blog/post_list.html
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ["title","content"]
+    #by defaul the postlistview looks for template in format <app>/<model>_<viewtype>.html
+    #for example: blog/post_list.html
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if(self.request.user == post.author):
+            return True
+        return False
+
+
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    #by defaul the postlistview looks for template in format <app>/<model>_<viewtype>.html
+    #for example: blog/post_list.html
+    
+    sucess_url = "/"
+
+    def test_func(self):
+        post=self.get_object()
+        if(self.request.user == post.author):
+            return True
+        return False
 
 def about(request):
     return render(request,"blog/about.html")
--- BlogApp/users/models.py
+++ BlogApp/users/models.py
@@ -16,6 +16,6 @@ class Profile(models.Model):
         super(Profile,self).save(*args,**kwargs)
         img = Image.open(self.image.path)
         if((img.height > 300) or (img.width > 300)):
-            output_size=(125,125)
+            output_size=(300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
